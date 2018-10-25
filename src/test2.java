33aba05cb4854f66e260a58253185d22330727d1
denis <deoniska@me.com>
2018-10-25 10:30:11 +0300

    new first 2

--- src/test2.java
+++ src/test2.java
@@ -1,6 +1,4 @@
-import javax.xml.bind.SchemaOutputResolver;
-
-public class giHub {
+public class test2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
